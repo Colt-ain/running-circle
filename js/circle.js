b9a4ac652dcf1937c98bfbdd78384a3d0ec8f605
colt-ain <ro7bps@gmail.com>
2017-05-05 16:33:54 +0300

    added background & rotate

--- js/circle.js
+++ js/circle.js
@@ -6,11 +6,13 @@ var target = container.querySelector('.target');
 var CICLE_RADIUS = 150 - 50;
 var jumpTarget = function () {
     var fi = Math.random() * 2 * Math.PI;
-    console.log('evt');
-    target.style.transition = '100ms';
+    target.style.transition = '200ms';
+    target.style.transform = 'rotate(' + (fi * 100) + 'deg)';
     target.style.top = ( CICLE_RADIUS + CICLE_RADIUS * Math.cos(fi)) + 'px';
     target.style.left = ( 25 + CICLE_RADIUS - CICLE_RADIUS * Math.sin(fi)) + 'px';
+
     target.addEventListener('mousemove', jumpTarget);
+
 };
 
 target.addEventListener('mouseover', jumpTarget);
